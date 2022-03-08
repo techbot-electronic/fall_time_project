61a688bc15c1e42d10a0f206fd956fb0ccdaba77
pham hong nham <70361369+hongnham@users.noreply.github.com>
2022-03-09 00:27:02 +0700

    Update button_and_relay.cpp

--- button_and_relay.cpp
+++ button_and_relay.cpp
@@ -1,11 +1,16 @@
+#define RELAY 4
+#define CB_RUNG A0
+#define BUTTON 2
+#define ECHO 7        // Pin received echo pulse
+#define TRIGGER 8     // Pin sends shot pulse 
+
+#define BAT_RELAY digitalWrite(RELAY, HIGH)
+#define TAT_RELAY digitalWrite(RELAY, LOW)
+
 #include <LiquidCrystal_I2C.h>
 
 #include <Wire.h> 
-//#include <NewPing.h>
 
-int button = 3;
-int relay = 4;
-int cbRung = 1;
 
  
 //#define TRIGGER_PIN 11
@@ -18,16 +23,15 @@ int cbRung = 1;
 
 LiquidCrystal_I2C lcd(0x27,16,2); 
 
-long duration, distance; // Duration used to calculate distance
 
 // cau hinh 0.5 s
-unsigned long interval = 10, count = 0;
+unsigned long interval = 1, count = 0;
 unsigned long tag = 0, debounce = 0;
 
 int TIME_OUT = 0;
 
 bool flg = false;
-bool todo = false;
+int todo = 0;
 
 
 void setup() {
@@ -41,57 +45,74 @@ void setup() {
   
   // put your setup code here, to run once:
   // Cau hinh nut nhan nhan tin hieu dau vao
-  pinMode(button, INPUT);
-  pinMode(cbRung, INPUT);
+  pinMode(BUTTON, INPUT);
+  pinMode(CB_RUNG, INPUT);
 
   // Cau hinh dieu khien relay
-  pinMode(relay, OUTPUT);
+  pinMode(RELAY, OUTPUT);
+  pinMode(TRIGGER,OUTPUT);   // chân trig sẽ phát tín hiệu
+  pinMode(ECHO,INPUT);    // chân echo sẽ nhận tín hiệu
 
-  digitalWrite(relay, HIGH);
- 
+
+  BAT_RELAY;
 
 
 }
 
 void loop() {
-
- int buttonStatus = digitalRead(button);
-  int cbRung = analogRead(cbRung);
- 
-   Serial.println(cbRung);
+//
+//unsigned long currentMillis = micros();
+//digitalWrite(TRIGGER, LOW);
+//
+//if (currentMillis - previousMillis > 4) {
+//  digitalWrite(TRIGGER, HIGH);
+//  previousMillis = currentMillis;
+//}
+//
+//if (currentMillis - previousMillis > 12) {
+//  digitalWrite(TRIGGER, LOW);
+//  previousMillis = currentMillis;
+//}
+//
+//duration = pulseIn(ECHO, HIGH);
+//distance_sm = impulseTime / 58; // convert to centimeters
+//distance = distance_sm;
   
- // unsigned int distance = sonar.ping_cm();
-  String disp = String("123");
-  String result = String(count * 10);
+//unsigned long duration; // biến đo thời gian
+    int distance;           // biến lưu khoảng cách
+    
+    /* Phát xung từ chân trig */
+//    digitalWrite(TRIGGER,0);   // tắt chân trig
+//    delayMicroseconds(2);
+//    digitalWrite(TRIGGER,1);   // phát xung từ chân trig
+//    delayMicroseconds(5);   // xung có độ dài 5 microSeconds
+//    digitalWrite(TRIGGER,0);   // tắt chân trig
+//    
+//    /* Tính toán thời gian */
+//    // Đo độ rộng xung HIGH ở chân echo. 
+//    duration = pulseIn(ECHO,HIGH);  
+//    // Tính khoảng cách đến vật.
+//    distance = int(duration/2/29.412);
 
-   if (cbRung < 500 && !todo ) {
-      Serial.println("HERE ---->>>");
-      lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
-      lcd.print("Hello world!");
-      lcd.clear();
-      lcd.print("t = "); // first line 
-      lcd.setCursor(4, 0); // second line
-      lcd.print(result);
   
-    }
 
-  
+ int buttonStatus = digitalRead(BUTTON);
+  int cbRung = analogRead(CB_RUNG);
+
   if (debounce > 0) debounce--;
 
   if (buttonStatus == 1 && debounce == 0) {
-    Serial.println("TAT RELAY");
-    todo = false;
+    todo = 1;
     debounce = 200;
-    digitalWrite(relay, LOW); // tat 
+    TAT_RELAY; // tat 
     tag = millis(); // co bat dau dem
   }
 
-  if (buttonStatus == 0 && debounce == 0 && !todo) {
+  if (buttonStatus == 0 && debounce == 0 && todo == 1) {
       
-      todo = true;
+      todo = 2;
       debounce = 200;
-      Serial.println("BAT RELAY");
-      digitalWrite(relay, HIGH);  // bat
+      BAT_RELAY;  // bat
   }
   
  
@@ -100,6 +121,31 @@ void loop() {
     count += 1;    
   }
   
+     if (cbRung < 500  && todo == 2) {
+        int totalMs = count * 10;
+        float second = (float)totalMs / 1000.0;
+        String result = String(second);
+        todo = 0;
+        
+        Serial.print(result);
+        Serial.println(" s");
+
+        lcd.clear();
+        lcd.setCursor(0,0);   
+        lcd.print("t = "); 
+        lcd.setCursor(4, 0); 
+        lcd.print(result + "s");
+  
+        lcd.setCursor(0,1);   
+        lcd.print("h = "); 
+        lcd.setCursor(4, 1); 
+        lcd.print(String(distance) + "cm");
+  
+        count = 0;
+
+  
+    }
+
    
 
 
